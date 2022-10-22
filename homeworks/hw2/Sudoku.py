9f148e1d22cb4686c249c32ddd4a57622b1273e1
udaya <udayag1@umbc.edu>
2022-10-22 18:53:58 -0400

    FINALLY DONE WITH CODE

--- homeworks/hw2/Sudoku.py
+++ homeworks/hw2/Sudoku.py
@@ -1,23 +1,23 @@
-#https://github.com/speix/sudoku-solver
-#https://github.com/stressGC/Python-AC3-Backtracking-CSP-Sudoku-Solver
 import itertools
-import sys
 #sources used
+#https://norvig.com/sudoku.html
 #http://aima.cs.berkeley.edu/python/csp.html
 #https://sandipanweb.wordpress.com/2017/03/17/solving-sudoku-as-a-constraint-satisfaction-problem-using-constraint-propagation-with-arc-consistency-checking-and-then-backtracking-with-minimum-remaning-value-heuristic-and-forward-checking/
 #https://www.techwithtim.net/tutorials/python-programming/sudoku-solver-backtracking/
+#https://www.w3schools.com/python/ref_func_sorted.asp
+#https://github.com/aimacode/aima-python/blob/master/csp.py
+#https://docs.python.org/3/library/itertools.html#itertools.combinations
+#https://rebec.casa/2019/01/13/a-python-sudoku-solver/
+#https://steven.codes/blog/constraint-satisfaction-with-sudoku/
 rows = "ABCDEFGHI"
 columns = "123456789"
 row_blocks = ("ABC","DEF","GHI")
 column_blocks = ("123","456","789")
-#characters = "ABCDEFGHI"
-#numbers = "123456789"
-
 class Sudoku:
     """
     Sudoku Representation:
       1 2 3 4 5 6 7 8 9
-    A . . . . . . . . .
+    A . . . . . . . . . (A1,A2,A3,A4,A5,A6,A7,A8,A9)
     B . . . . . . . . .
     C . . . . . . . . .
     D . . . . . . . . .
@@ -29,75 +29,57 @@ class Sudoku:
     AC3:
     """
     def __init__(self, grid):
-        self.variables = []
-        self.domains = {}
-        self.constraints = []
-       
-        board = list(grid)
+        board = []
+        for c in grid:
+            board.append(c)
         #intiialize variables for each box, "A1,A2,A3..."
         self.variables = self.concatenate(rows,columns)
-        #print(len(self.variables))
-        
-        #print("_____________________________________")
         #initializing possible domain for each box in sudoku grid
+        self.domains = {}
         for index,box in enumerate(self.variables):
-            if board[index] == '0':
+            if board[index] == '.':
                 self.domains[box] = list(range(1,10))
             else:
                 self.domains[box] = [int(board[index])]
-        
-        #self.domains = {v: list(range(1, 10)) if board[i] == '0' else [int(board[i])] for i, v in enumerate(self.variables)}
-
         self.constraints = self.getBoardConstraints()
-        #print(self.constraints)
-        #print(len(self.constraints))
-        #neighbor values
         self.neighbors = {}
         for v in self.variables:
             self.neighbors[v] = []
             for c in self.constraints:
-                if v == c[0]:
-                    self.neighbors[v].append(c[1])
-        #pruning values
+                (x,y) = (c[0],c[1])
+                if x == v:
+                    self.neighbors[v].append(y)
         self.pruned = {}
         for index,var in enumerate(self.variables):
-            if board[index] == '0':
+            if board[index] == '.':
                 self.pruned[var] = []
             else:
                 self.pruned[var] = int(board[index])
-        #print("PRUNED")
-        #print(self.pruned)
-        #print(len(self.pruned))
-
-    def fc(self, box, val, assignment):
-        for n in self.neighbors[box]:
+    #add the boxes value to assignment 
+    def assign(self, var,val,assignment):
+        assignment[var] = val
+        #starts forward check to prune values
+        for n in self.neighbors[var]:
             if n not in assignment:
                 if val in self.domains[n]:
                     self.domains[n].remove(val)
-                    self.pruned[box].append((n,val))
-
-    def assign(self, var,val,assignment):
-        assignment[var] = val
-        self.fc(var,val,assignment)
+                    self.pruned[var].append((n,val))
+    #remove the boxes value from the assignment
     def unassign(self, box, assignment):
         if box in assignment:
-            for (x,y) in self.pruned[box]:
+            for x,y in self.pruned[box]:
                 self.domains[x].append(y)
             self.pruned[box] = []
             del assignment[box]
-    def isConsistent(self, assignment, value, box ):
-        consistency = True
-        for key,val in assignment.items():
-            if val == value and key in self.neighbors[box]:
-                consistency = False
-        return consistency
-
+    #If a domain of a variable is >1 that means Sudoku is not solved     
     def isDone(self):
         for x in self.variables:
             if len(self.domains[x]) > 1:
                 return False
         return True
-
+    #This function gets the row, column, and 3x3 constraints
+    #essentially the rules of sudoku
+    #Then converts into binary constraints for AC3 algorithm
     def getBoardConstraints(self):
         c1 = self.getColumnConstraints()#A1,B1,C1...
         c2 = self.getRowConstraints()#A1,A2,A3,A4...
@@ -113,9 +95,13 @@ class Sudoku:
                 if arr not in all_binary_constraints:
                     all_binary_constraints.append([arr[0],arr[1]])
         return all_binary_constraints
-
-
-
+    #checks all constraints
+    def isConsistent(self, assignment, value, box ):
+        consistency = True
+        for key,val in assignment.items():
+            if val == value and key in self.neighbors[box]:
+                consistency = False
+        return consistency
     def getColumnConstraints(self):
         arr = []
         for c in columns:
@@ -131,134 +117,122 @@ class Sudoku:
         for r in row_blocks:
             for c in column_blocks:
                 arr.append(self.concatenate(r, c))
-        return arr
-        
+        return arr     
     #helper functions
+    #makes the sudoku boards coordinates
     def concatenate(self,x,y):
         arr = []
         for i in x:
             for j in y:
                 arr.append(i+j)
         return arr
-    
     def getNumOfConflicts(self,s,box,val):
         c = 0
         for n in s.neighbors[box]:
             if len(s.domains[n]) > 1 and val in s.domains[n]:
                 c = c + 1
         return c
-
-
-def isNotEqual(x,y):
-    return x!=y
-
 def revise(sudoku, xi, xj):
     revised = False
     for x in sudoku.domains[xi]:
-        #print(x)
-        #print("HEllo")
-        if not any([isNotEqual(x,y) for y in sudoku.domains[xj]]):
+        remove_element = True
+        for y in sudoku.domains[xj]:
+            if x != y:
+                remove_element = False         
+        if remove_element:
+            revised = True
             sudoku.domains[xi].remove(x)
-            revised = True
-        
     return revised
-    
-"""
-def revise(s,x,y):
-    revised = False
-    for d in s.domains[x]:
-        if not any([s.isNotEqual(d,b) for b in s.domains[y]]):
-            s.domains[x].remove(d)
-            revised = True
-    return revised
-
-"""
-
 def ac3(sudoku_board):
-    print("PLEASE TELL ME WE REACHED HERE")
     queue = list(sudoku_board.constraints)
     while queue:  
         i,j = queue.pop(0)
-        #print(i,j)
         if revise(sudoku_board,i,j):
-            #print("after if")
             if len(sudoku_board.domains[i]) == 0:
-                print("Okay HOW ABOUT HERE")
                 return False  
             for k in sudoku_board.neighbors[i]:
                 if k != i:
                     queue.append([k,i])
-    
     return True
-
-
-
-def backtrack(assignment, sudoku):
-    if len(assignment) == len(sudoku.variables):
+def backtrack(assignment, puzzle):
+    if len(assignment) == len(puzzle.variables):
         return assignment
-    var = select_unassigned_variable(assignment, sudoku)
-    for value in order_domain_values(sudoku, var):
-        if sudoku.isConsistent(assignment, value, var):
-            sudoku.assign(var, value, assignment)
-            result = backtrack(assignment, sudoku)
+    var = mcv(assignment, puzzle)
+    for value in lcv(puzzle, var):
+        if puzzle.isConsistent(assignment, value, var):
+            puzzle.assign(var, value, assignment)
+            result = backtrack(assignment, puzzle)
             if result:
                 return result
-            sudoku.unassign(var, assignment)
+            puzzle.unassign(var, assignment) 
     return False
-
-
-# Most Constrained Variable heuristic
-# Pick the unassigned variable that has fewest legal values remaining.
-def select_unassigned_variable(assignment, sudoku):
-    unassigned = [v for v in sudoku.variables if v not in assignment]
+# Most Constrained Variable
+def mcv(assignment, sudoku):
+    unassigned = []
+    for v in sudoku.variables:
+        if v not in assignment:
+            unassigned.append(v)
     return min(unassigned, key=lambda var: len(sudoku.domains[var]))
-
-# Least Constraining Value heuristic
-# Prefers the value that rules out the fewest choices for the neighboring variables in the constraint graph.
-def order_domain_values(sudoku, var):
+# Least Constrained Value
+def lcv(sudoku, var):
     if len(sudoku.domains[var]) == 1:
         return sudoku.domains[var]
     return sorted(sudoku.domains[var], key=lambda val: sudoku.getNumOfConflicts(sudoku, var, val))
+def print_board(boardObj, solved):
+    print()
+    c = 0
+    d = 0
+    for b in boardObj.variables:
+        if solved:
+            print(boardObj.domains[b][0], end = " ")
+        else:
+            print(boardObj.domains[b], end = " ")
+        c+=1
+        d+=1
+        if c % 3 == 0 and c != 9 :
+            print("|", end = " ")
+        if c % 9 == 0:
+            c = 0
+            print()
+            if d%27 == 0:
+                d = 0
+                print("----------------------")
+    print()
+def print_str(sudoku_str):
+    print()
+    c = 0
+    d = 0
+    for s in sudoku_str:
+        print(s, end = " ")
+        c+=1
+        d+=1
+        if c % 3 == 0 and c != 9:
+            print("|", end = " ")
+        if c % 9 == 0:
+            c = 0
+            print()
+            if d % 27 == 0:
+                d = 0
+                print("----------------------")
+    print()
 ##########################################################################################################
 if __name__ == "__main__":
     p1 = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
     p2 = "...26.7.168..7..9.19...45..82.1...4...46.29...5...3.28..93...74.4..5..367.3.18..."
-    p1 = p1.replace(".","0")
-    p2 = p2.replace(".","0")
-    p3 = "000530000005000600000190503000004000000000164100370800008000040010000008004700921"
-    print(len(p3))
+    p3 = "4.....3.....8.2......7........1...8734.......6........5...6........1.4...82......"
     test = Sudoku(p3)
-    #print(test.domains)
-    print("_______________________________________________")
-    if ac3(test):
-        print("We made it this far")
-        if test.isDone():
-            print(test.variables)
-            c = 0
-            for v in test.variables:
-                print(test.domains[v][0], end = " ")
-                c+= 1
-                if c%9 == 0:
-                    print()
-        else:
-            print("Hey atleast u tried")
-            assignment = {}
-            for x in test.variables:
-                if len(test.domains[x]) == 1:
-                    assignment[x] = test.domains[x][0]
-            assignment = backtrack(assignment, test)
-            for d in test.domains:
-                test.domains[d] = assignment[d] if len(d) > 1 else test.domains[d]
-            if assignment:
-                print("ALMOST DONE")
-                #print(test.variables)
-                c = 0
-                for v in test.variables:
-                    print(test.domains[v], end = " ")
-                    c+= 1
-                    if c%9 == 0:
-                        print()
-
-
-
-            
+    print("Sudoku Board Puzzle 1 -- Before Solving")
+    print_str(p3)
+    arc_consistency = ac3(test)
+    done = test.isDone()
+    if done:
+        print("AC3 WORKS")
+        print_board(test,done)
+    else:
+        print("AC3 FAILED")
+        print("STARTING THE BACKTRACK")
+        assignment = {}
+        assignment = backtrack(assignment, test)
+        for d in test.domains:
+            test.domains[d] = assignment[d]
+        print_board(test, done)
